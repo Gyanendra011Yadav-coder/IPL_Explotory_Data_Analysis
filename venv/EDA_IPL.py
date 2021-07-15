c678544ce62abd0206977848afc488316b0d7623
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-15 10:23:17 -0700

    READING THE csv FILE, BY GIVING THE ADDRESS OF THE FILE.

--- venv/EDA_IPL.py
+++ venv/EDA_IPL.py
@@ -2,3 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 
+#READING THE FILE, BY PROVIDING THE ADDRESS OF THE FILE IN THE SYSTEM
+read_data=pd.read_csv('C:\\Users\\Lenovo\\OneDrive\\Desktop\\Data Sets For Analysis\\IPL DATA\\matches.csv')
+#USING THIS pd.set_set_option, WE ARE JUST MAKING THE ALL ROWS AND COLUMNS PRINT COMPLETLY
+pd.set_option('display.max_columns',None)
+pd.set_option('display.max_rows',None)
+#PRINTING THE DATA OF THE CSV FILE
+print(read_data.head(2))
+
